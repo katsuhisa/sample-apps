96c461fb38666c9bc9dafe02c7d93ba5a914fd76
kdeto-uelab <deto.ue.see.eng.osaka-u.ac.jp>
2023-10-18 10:52:25 +0900

    script.jsを追加

---
+++ blog-apps/script.js
@@ -0,0 +1,6 @@
+async function loadPosts() {
+    const response = await fetch('/get-posts.php');
+    const posts = await response.json();
+    const container = document.getElementById('blog-posts');
+    container.innerHTML = posts.map(post => `<p>${post.title}: ${post.content}</p>`).join('');
+}
